421c3072e78711707059e25d8abdb66b8b6e6bc5
hcurotta <hcurotta@gmail.com>
2012-09-06 14:09:32 -0500

    removed env variables

--- config/initializers/_ENV.rb
+++
@@ -1,5 +0,0 @@
-ENV["twitter_key"] = 'W65ECsRxhytTRnmCabiZA'
-ENV["twitter_secret"] = 'U4gEVJHhhN18tiJc0SOBn0NpzWpZWelGBAontHS48M'
-
-ENV["facebook_key"] = '460912570598243'
-ENV["facebook_secret"] = 'f45b3c8cc9fbece36b9d95cd472434ce'
